02c3a02fb5b1e26693a9aa2c6ab8367fa03a5e45
Victor Olabode <victonytechsolution@gmail.com>
2024-08-21 14:07:23 -0700

    uploading bool file

---
+++ c program/bool.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main()
+{
+	int x = 5;
+	int y = 3;
+	printf("%d\n", x>y); // This return 1 as true because 5>3
+
+	// create boolean variables
+	bool programmingFunc = true;
+	bool isTasty = false;
+	//return boolean value
+
+	printf("%d\n", programmingFunc);
+	printf("%d\n", isTasty);
+	return 0; 
+}
