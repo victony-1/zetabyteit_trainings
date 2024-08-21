bbb7a0ac4593650216cb7bd33251b8b4817cbd90
Victor Olabode <victonytechsolution@gmail.com>
2024-08-21 15:57:09 -0700

    uploading dimension file

---
+++ python program/dimension.py
@@ -0,0 +1,21 @@
+dimension = (200, 50);
+print(dimension[0])
+print(dimension[1])
+
+figure = (3,)
+print(figure[0])
+
+dimensions = (200, 50)
+for dimension in dimensions:
+	print(dimension)
+    >
+
+measures = (300, 100)
+print("original dimension")
+for measure in measures:
+	print (measure)
+
+measures = (400, 200)
+print("\n modified dimension")
+for measure in measures: 
+	print(measure)		
