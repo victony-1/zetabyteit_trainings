3d66471236a6193037cfefedac098c913761135a
Victor Olabode <victonytechsolution@gmail.com>
2024-08-21 13:42:43 -0700

    adding condition file

---
+++ python program/condition.py
@@ -0,0 +1,15 @@
+cars = ["benz", "toyota", "ferrari", "G-wagon","corrola"]
+for car in cars:
+	print(car)
+	# writing a conditional statement
+
+my_users = "femi", "kola", "david", "daniel"
+new_user = "ade"
+if new_user not in my_users:
+	print(f"{new_user.title()},you can put a respond straight away")
+
+
+#boolean expression
+game_active = True
+can_edit = False
+
