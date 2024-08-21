8b256e9e24f77d1c9768ccb95a83ce7c74d09ab5
Victor Olabode <victonytechsolution@gmail.com>
2024-08-21 13:41:16 -0700

    adding another task

---
+++ python program/clw1.py
@@ -0,0 +1,20 @@
+alien_color = ["green player", "yellow player", "red player"]
+if 'green player' in alien_color:
+	print("A green player just earn five points")
+if 'purple player' in alien_color:
+	print("A purple player just earn six points")
+
+age = 16
+if 	age<2:
+	print("You are a child")
+if	age<4:
+	print("You are a toddler")
+if age<13:
+	print("You are a kid")	
+if age<20:
+	print("You are still a teenager")
+if age<65:
+	print("You are an adult")
+if age>=65:
+	print("you are an elder")				
+	
