f3830065fce09f0baa379da85bcaf84c5082ab1f
Victor Olabode <victonytechsolution@gmail.com>
2024-08-21 13:45:27 -0700

    loading def file

---
+++ python program/countries.py
@@ -0,0 +1,14 @@
+def countries(country = "Norway"):
+	print("I am from" + country)
+countries("Brazil")
+countries("Nigeria")
+countries("Germany")
+countries()
+countries("chile")	
+
+print("\n")
+def food_store(food):
+	for x in food:
+		print(x)
+fruits = ["apple", "mango", "pineapple", "water lemon", "carrot"]
+food_store(fruits)		
