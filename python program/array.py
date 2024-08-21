1a42997394f2419b2433fec9d8dc54830a4a7454
Victor Olabode <victonytechsolution@gmail.com>
2024-08-21 13:18:38 -0700

    uploading an array file

---
+++ python program/array.py
@@ -0,0 +1,23 @@
+car_names = ["Ford", "Volvo", "Bmw", "Benz", "Toyota"]
+print(car_names)
+
+x = car_names[0]
+print(x)
+
+print(car_names[0])
+
+x = len(car_names)
+print(x)
+
+car_names = ["Ford", "Volvo", "Bmw", "Benz", "Toyota"]
+for x in car_names:
+	print(x)
+
+car_names.append("Honda")
+print(car_names)	
+
+car_names.pop(1)
+print(car_names)
+
+car_names.remove("Benz")
+print(car_names)
